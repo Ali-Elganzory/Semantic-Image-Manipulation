6eb5a9d43fd7bb142d991fcee65962bfc319a728
Ali Elganzory <alielganzory@hotmail.com>
2023-04-24 14:07:16 +0200

    Update download destination in download_models.ps1

--- download_models.ps1
+++ download_models.ps1
@@ -1,9 +1,9 @@
 # YOLO
 $uri = "https://drive.google.com/uc?id=1oejmnuTuP_-s7wK100cKiMWa9rwd7Do7&export=download"
-$path = "backend/ml/yolo/detect_yolov8x.pt"
+$path = "backend/backend/ml/yolo/detect_yolov8x.pt"
 Invoke-WebRequest -Uri $uri -OutFile $path
 
 # LaMa
 $uri = "https://drive.google.com/uc?id=1lf6rliY2leI7cnEbAQO0NZ08YRg2HktA&export=download"
-$path = "backend/ml/lama/models/best.ckpt"
+$path = "backend/backend/ml/lama/models/best.ckpt"
 Invoke-WebRequest -Uri $uri -OutFile $path
