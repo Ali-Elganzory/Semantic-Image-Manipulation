ceaf91d1f52f35c0faa027d3c362f31e5e90105d
Ali Elganzory <ali.elganzory@alooba.com>
2023-06-18 15:49:03 +0300

    Formatted YOLO.py

--- backend/backend/ml/yolo/YOLO.py
+++ backend/backend/ml/yolo/YOLO.py
@@ -8,7 +8,7 @@ from ultralytics.yolo.engine.results import Results
 
 
 class ModelSize:
-    XLarge = 'yolov8x'
+    XLarge = "yolov8x"
 
 
 @dataclass
@@ -54,10 +54,12 @@ class BBox:
         return self.x1, self.y1, self.x2 - self.x1, self.y2 - self.y1
 
     def scaled(self, scale: float) -> BBox:
-        return BBox(*scaled(self.xyxy, scale),
-                    self.confidence,
-                    self.label_id,
-                    self.label)
+        return BBox(
+            *scaled(self.xyxy, scale),
+            self.confidence,
+            self.label_id,
+            self.label,
+        )
 
 
 class YOLO:
@@ -65,7 +67,8 @@ class YOLO:
         self.model_size = model_size
         self.path = Path(__file__).parent
         self.detection_model = YOLOUltra(
-            model=f"{self.path}/detect_{self.model_size}.pt")
+            model=f"{self.path}/detect_{self.model_size}.pt"
+        )
         # self.segmentation_model = YOLOUltra(
         #     model=f"{self.path}/segment_{self.model_size}.pt")
         self.class_names = self.detection_model.names
@@ -75,38 +78,50 @@ class YOLO:
         return [*self.class_names]
 
     def detect(
-            self,
-            images: Union[List[np.ndarray], np.ndarray],
-            label_ids: List[int] = [],
+        self,
+        images: Union[List[np.ndarray], np.ndarray],
+        label_ids: List[int] = [],
     ) -> Union[List[List[BBox]], List[BBox]]:
         if isinstance(images, np.ndarray):
             images = [images]
 
         results: Iterable[Results] = map(
-            lambda x: x.cpu().numpy(), self.detection_model(images))
-        bounding_boxes = [[BBox(*map(lambda x: int(x), result.boxes.xyxy[i]),
-                                result.boxes.conf[i],
-                                int(result.boxes.cls[i]),
-                                self.class_names[int(result.boxes.cls[i])])
-                           for i in range(len(result.boxes))]
-                          for result in results]
+            lambda x: x.cpu().numpy(), self.detection_model(images)
+        )
+        bounding_boxes = [
+            [
+                BBox(
+                    *map(lambda x: int(x), result.boxes.xyxy[i]),
+                    result.boxes.conf[i],
+                    int(result.boxes.cls[i]),
+                    self.class_names[int(result.boxes.cls[i])],
+                )
+                for i in range(len(result.boxes))
+            ]
+            for result in results
+        ]
 
         if len(label_ids) > 0:
-            bounding_boxes = [[box for box in boxes if box.label_id in label_ids]
-                              for boxes in bounding_boxes]
+            bounding_boxes = [
+                [box for box in boxes if box.label_id in label_ids]
+                for boxes in bounding_boxes
+            ]
 
         return bounding_boxes[0] if len(bounding_boxes) == 1 else bounding_boxes
 
-    def segment(self, image: np.ndarray):
-        pass
 
-
-def scaled(coordinates: Tuple[int, int, int, int], scale: float) -> Tuple[int, int, int, int]:
+def scaled(
+    coordinates: Tuple[int, int, int, int], scale: float
+) -> Tuple[int, int, int, int]:
     coordinates = np.array(coordinates, dtype=np.float32)
-    translation = np.array([(coordinates[0] + coordinates[2]) / 2,
-                            (coordinates[1] + coordinates[3]) / 2,
-                            (coordinates[0] + coordinates[2]) / 2,
-                            (coordinates[1] + coordinates[3]) / 2])
+    translation = np.array(
+        [
+            (coordinates[0] + coordinates[2]) / 2,
+            (coordinates[1] + coordinates[3]) / 2,
+            (coordinates[0] + coordinates[2]) / 2,
+            (coordinates[1] + coordinates[3]) / 2,
+        ]
+    )
     coordinates -= translation
     coordinates *= scale
     coordinates += translation
